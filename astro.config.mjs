5c94279eb0b5120be1500a9af9bc1e29da838cf0
MakoStar <makostar.github@gmail.com>
2026-09-15 12:21:16 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ export default defineConfig({
   site: 'https://makostar.github.io/',
   base: '/ss-dating/',
   output: 'static',
+  vite: {
+    optimizeDeps: {
+      exclude: ['@makostar/ss-cursor'],
+    },
+  },
 });
